2b0081bd28112e84054f6fc39cfbcdf5a5228683
LuckyPants27 <hitori-a@yandex.ru>
2024-05-31 06:34:08 +0500

    Initial Commit

---
+++ src/main/java/pro/sky/mynewproject25/MyNewProject25Application.java
@@ -0,0 +1,13 @@
+package pro.sky.mynewproject25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewProject25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNewProject25Application.class, args);
+    }
+
+}
